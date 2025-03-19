f7c2ddae49ef5413f244a762c467e03d13e02d76
liz-nmr <liz@nomorerobots.io>
2025-03-19 18:27:52 +0000

    logger settings order

--- Project Search/Assets/Scripts/Utility/LoggerSettings.cs
+++ Project Search/Assets/Scripts/Utility/LoggerSettings.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Logger Settings", menuName = "ScriptableObjects/Settings/Logger Settings")]
+[CreateAssetMenu(fileName = "Logger Settings", menuName = "ScriptableObjects/Settings/Logger Settings", order = 100)]
 public class LoggerSettings : ScriptableObject
 {
     public bool EnableDigitSequenceLogs => _enableDigitSequenceLogs;
